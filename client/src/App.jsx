924a3b373175ba464ff2c4d9b88d23ccc0e8ae03
Victoria Canclini <vico_canclini@hotmail.com>
2023-09-06 11:16:16 -0300

    Agregue el componente Content.js

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,10 +2,12 @@ import React from "react";
 import "./App.css";
 import Navbar from "./components/Navbar";
 import AuthContextProvider from "./contexts/AuthContext";
+import Content from "./components/Content";
 
 const App = () => (
   <AuthContextProvider>
     <Navbar />
+    <Content />
   </AuthContextProvider>
 );
 
---
+++ client/src/components/Content.js
@@ -0,0 +1,37 @@
+import React from "react";
+import { Routes, Route } from "react-router";
+import RegistrationForm from "./RegistrationForm";
+import Login from "./Login";
+import { useContext } from "react";
+import { AuthContext } from "../contexts/AuthContext";
+import Cookies from "js-cookie";
+
+function Content() {
+  const { toggleAuth } = useContext(AuthContext);
+  const token = Cookies.get("token");
+
+  useEffect(() => {
+    if (token) {
+      axios
+        .get("/me") // ver la ruta del back
+        .then((response) => {
+          const user = response.data;
+          toggleAuth(user);
+        })
+        .catch((error) => {
+          console.error("Error al verificar el token:", error);
+        });
+    }
+  }, [token]);
+
+  return (
+    <div>
+      <Routes>
+        <Route path="/register" element={<RegistrationForm />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default Content;
