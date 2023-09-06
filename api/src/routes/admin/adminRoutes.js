fde93e36d6c46b0381fbc066d4db320d42a4cabc
Francisco-Villanueva <franciscovillanuevaj99@gmail.com>
2023-09-06 19:38:13 -0300

    rutas de admin para manejo de users

---
+++ api/src/routes/admin/adminRoutes.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const router = express.Router();
+
+const { getAllUsers, makeAdmin, deleteUser } = require("./services");
+router.get("/users", getAllUsers);
+router.put("/users/:userId", makeAdmin);
+router.delete("/users/:userId", deleteUser);
+
+module.exports = router;
