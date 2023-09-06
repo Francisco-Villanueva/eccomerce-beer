a9f803fd77a2b6db558d56c474757b7e798e50d9
Francisco-Villanueva <franciscovillanuevaj99@gmail.com>
2023-09-06 19:50:24 -0300

    inluir carrito sobre cada user

--- api/src/db/models/index.js
+++ api/src/db/models/index.js
@@ -4,12 +4,11 @@ const Book = require("./Book");
 const Cart = require("./Cart");
 const Cart_buy = require("./Cart_Buy");
 
-
-Cart.belongsTo(User, { foreignKey: "cartId" });
-Cart.hasMany(Cart_buy, { foreignKey: "cartId" });
-Book.belongsTo(Cart_buy, { foreignKey: "bookId" });
+User.hasMany(Cart, { as: "user_cart" });
+Cart.hasMany(Cart_buy, { as: "cartId" });
+Book.belongsTo(Cart_buy, { as: "bookId" });
 // User.hasOne(Cart, { foreignKey: "cartId" });
 // Cart_buy.belongsTo(Cart, { foreignKey: "cartId" });
 // Cart_buy.belongsTo(Book, { foreignKey: "bookId" });
 
-module.exports = {User, Book, Cart, Cart_buy};
+module.exports = { User, Book, Cart, Cart_buy };
