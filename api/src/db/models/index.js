a39d0c9856e9a52f88b4ef9f44472ad46c7642c7
Nicolas Fridman <nicofri2001@gmail.com>
2023-09-06 16:49:43 -0300

    first cart route created

--- api/src/db/models/index.js
+++ api/src/db/models/index.js
@@ -5,11 +5,11 @@ const Cart = require("./Cart");
 const Cart_buy = require("./Cart_Buy");
 
 
-User.hasOne(Cart, { foreignKey: "cartId" });
 Cart.belongsTo(User, { foreignKey: "cartId" });
 Cart.hasMany(Cart_buy, { foreignKey: "cartId" });
-Cart_buy.belongsTo(Cart, { foreignKey: "cartId" });
-Cart_buy.belongsTo(Book, { foreignKey: "bookId" });
-Book.hasMany(Cart_buy, { foreignKey: "bookId" });
+Book.belongsTo(Cart_buy, { foreignKey: "bookId" });
+// User.hasOne(Cart, { foreignKey: "cartId" });
+// Cart_buy.belongsTo(Cart, { foreignKey: "cartId" });
+// Cart_buy.belongsTo(Book, { foreignKey: "bookId" });
 
 module.exports = {User, Book, Cart, Cart_buy};
--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,9 +1,10 @@
 // cree el router el que une todas las rutas
 const express = require("express");
 const router = express.Router();
+const cartRouter = require("./Cart/cart.Routes");
 
 router.get("/test", (req, res) => {
   res.send("anda el server");
 });
-
+router.use("/cart", cartRouter);
 module.exports = router;
