aa4ffad7ac8868e54e0b6593db99683f35e65b0c
GRabinovich <garabinovich@gmail.com>
2023-09-07 14:54:01 -0300

    Terminada estructura de grilla y productos

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,16 +2,16 @@ import { useState } from "react";
 import "./App.css";
 import { Routes, Route } from "react-router-dom"
 import { AllProducts } from "./components/AllProducts";
+import { OneProduct } from "./components/OneProduct";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <h1>Beer</h1>
-      <h2>Nico</h2>
       <Routes>
         <Route path="/" element= {<AllProducts/>} />
+        <Route path="/user/products/:id" element= {<OneProduct/>} />
       </Routes>
     </>
   );
--- client/src/components/AllProducts.jsx
+++ client/src/components/AllProducts.jsx
@@ -1,40 +1,55 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { Link } from "react-router-dom";
-import { items } from "../mocks/books.json";
 
 export const AllProducts = () => {
-  const [books, setBooks] = useState(items);
+  const [books, setBooks] = useState([]);
 
-  console.log(items[0].volumeInfo.imageLinks.thumbnail);
-
-  // useEffect(() => {
-  //   axios
-  //     .get("/rutaMostrarTodosLosLibros")
-  //     .then((res) => {
-  //       setBooks(res.data.items);
-  //     })
-  //     .catch((error) => {
-  //       console.log(error);
-  //     });
-  // }, []);
+  useEffect(() => {
+    axios
+      .get("http://localhost:4000/user/products")
+      .then((res) => {
+        setBooks(res.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
 
   return (
-    <>
+    <div className="columns is-multiline is-centered">
       {books.map((book) => {
         const thumbnail = book.volumeInfo.imageLinks
           ? book.volumeInfo.imageLinks.smallThumbnail
-          : "https://cdn-icons-png.flaticon.com/512/3068/3068077.png";
-
+          : "https://cdn-icons-png.flaticon.com/512/2421/2421033.png";
+  
         return (
-          <Link to={`/${book.id}`} key={book.id} style={{textDecoration: "none", color: "inherit"}}>
-            <div>
-              <img src={thumbnail} alt={book.volumeInfo.title} style={{width: "140px", height: "183px"}}/>
-              <p>{book.volumeInfo.title}</p>
-            </div>
-          </Link>
+          <div className="column is-one-fifth" key={book.id}>
+            <Link
+              to={`user/products/${book.id}`}
+              style={{ textDecoration: "none", color: "inherit" }}
+            >
+              <div className="card">
+                <div className="card-image">
+                  <figure className="image is-4by3">
+                    <img
+                      src={thumbnail}
+                      alt={book.volumeInfo.title}
+                      style={{ height: "366px" }}
+                    />
+                  </figure>
+                </div>
+                <div className="card-content">
+                  <div className="content">
+                    {book.volumeInfo.title}
+                  </div>
+                </div>
+              </div>
+            </Link>
+          </div>
         );
       })}
-    </>
+    </div>
   );
 };
+
