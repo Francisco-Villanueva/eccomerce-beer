0786bd87da33a147ce3f87dec26e54f0d9d6f528
Francisco-Villanueva <franciscovillanuevaj99@gmail.com>
2023-09-07 15:45:53 -0300

    cambiso en el admin back

--- api/src/routes/admin/adminRoutes.js
+++ api/src/routes/admin/adminRoutes.js
@@ -1,9 +1,14 @@
 const express = require("express");
 const router = express.Router();
 
-const { getAllUsers, makeAdmin, deleteUser } = require("./services");
+const {
+  getAllUsers,
+  makeAdmin,
+  deleteUser,
+  getUserById,
+} = require("./services");
 router.get("/users", getAllUsers);
-router.get("/users/:userId", getAllUsers);
+router.get("/users/:userId", getUserById);
 router.put("/users/:userId", makeAdmin);
 router.delete("/users/:userId", deleteUser);
 
