b138467e025d2aec35953321aa505070abf41a13
GRabinovich <garabinovich@gmail.com>
2023-09-08 15:10:02 -0300

    nn

--- client/src/components/Cart.jsx
+++ client/src/components/Cart.jsx
@@ -6,14 +6,72 @@ import { CartBooksContext } from "../contexts/CartBookContext";
 export const Cart = () => {
   const { cartBooks } = useContext(CartBooksContext);
 
-  console.log(cartBooks);
-
   return (
     <div>
-      {cartBooks[0]?.map((m, index) => (
-        <p key={index}>{m.volumeInfo?.title}</p>
+      {cartBooks[0]?.map((book, index) => (
+        <div key={index}>
+          <div className="individual" style={{ width: "820px" }}>
+            <div className="card-content">
+              <div className="columns">
+                <div className="column is-one-third">
+                  <img
+                    src={
+                      book.volumeInfo.imageLinks?.smallThumbnail ||
+                      "https://cdn-icons-png.flaticon.com/512/2421/2421033.png"
+                    }
+                    alt={book.volumeInfo.title}
+                    style={{ height: "366px" }}
+                  />
+                </div>
+                <div className="column">
+                  <h1 className="title is-3">
+                    {book.volumeInfo.title || "No Title"}
+                  </h1>
+                  <p>
+                    <span className="title is-6">Description:</span>{" "}
+                    {book.volumeInfo.description
+                      ? book.volumeInfo.description
+                          .split("<br>")
+                          .join("")
+                          .slice(0, 150) + "..."
+                      : "No Description"}
+                  </p>
+                  <p>
+                    <span className="title is-6">Authors:</span>{" "}
+                    {book.volumeInfo.authors?.join(", ") || "Not Authors"}
+                  </p>
+                  {/* <p>{book.volumeInfo.price}</p> */}
+                  <p>
+                    <span className="title is-6">Release Date:</span>{" "}
+                    {book.volumeInfo.publishedDate || "Unknown"}
+                  </p>
+                  <p>
+                    <span className="title is-6">Number of pages:</span>{" "}
+                    {book.volumeInfo.pageCount || "Unknown"}
+                  </p>
+                  <p>
+                    <span className="title is-6">Publisher:</span>{" "}
+                    {book.volumeInfo.publisher || "Unknown"}
+                  </p>
+                  <p>
+                    <span className="title is-6">Categories:</span>{" "}
+                    {book.volumeInfo.categories?.join(", ") || "Not Categories"}
+                  </p>
+                  <p>
+                    <span className="title is-6">Language:</span>{" "}
+                    {book.volumeInfo.language || "Unknown"}
+                  </p>
+                  <p>
+                    <span className="title is-6">Price:</span>{" "}
+                    {`$${book.price}` || "Unknown"}
+                  </p>
+                  {/* <input /> */}
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       ))}
     </div>
   );
 };
-
