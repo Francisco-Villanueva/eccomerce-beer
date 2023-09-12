5104677286fae02f95fdcc43eeb097b8eb9ba60b
Nicolas Fridman <nicofri2001@gmail.com>
2023-09-12 15:54:20 -0300

    navbar styles3

--- client/src/commons/Cards/Loading.jsx
+++ client/src/commons/Cards/Loading.jsx
@@ -1,5 +1,9 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSpinner } from "@fortawesome/free-solid-svg-icons";
 
 export default function Loading() {
-  return <div>Loading...</div>;
+  return <div className="loading">
+    <p><FontAwesomeIcon icon={faSpinner} spin size="1.9x"/>  Loading...</p>
+    </div>;
 }
