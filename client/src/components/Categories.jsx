7e454b95d8468c07f0834609ffb9e7edeb3e24f0
Victoria Canclini <vico_canclini@hotmail.com>
2023-09-14 15:42:15 -0300

    aa

--- client/src/components/Categories.jsx
+++ client/src/components/Categories.jsx
@@ -43,26 +43,58 @@ function Categories({ setSelectedCategories }) {
     } = event;
 
     setSelectedCategoriesLocal(value);
-
+    
     setSelectedCategories(value);
   };
 
-  // ...
-
   return (
-    <div>
-      <FormControl sx={{ m: 1, width: 300 }}>
-        <InputLabel id="demo-multiple-checkbox-label">Categories</InputLabel>
+    <div style={{ backgroundColor: "#535353" }}>
+      <FormControl
+        sx={{ m: 0, width: 200 }}
+        style={{ backgroundColor: "#535353", padding: 0, display: "flex"}}
+      >
+        <InputLabel
+          id="demo-multiple-checkbox-label"
+          style={{
+            backgroundColor: "transparent",
+            color: "#fff",
+            fontFamily: "'Hanken Grotesk', sans-serif",
+            padding: 0,
+            margin: 0,
+            textAlign: "center",
+            alignItems: "center",
+            display: "flex",
+            lineHeight: "normal",
+            zIndex: 2,
+            // maxHeight: "6vh",
+            // bottom: "20%", 
+            // position: "absolute"
+          }}
+        >
+          Categories
+        </InputLabel>
         <Select
           labelId="demo-multiple-checkbox-label"
           id="demo-multiple-checkbox"
           multiple
           value={selectedCategories}
           onChange={handleChange}
-          input={<OutlinedInput label="Tag" />}
+          input={<OutlinedInput label="Categories" />}
           renderValue={(selected) => selected.join(", ")}
           MenuProps={MenuProps}
+          style={{ 
+            backgroundColor: "#535353",
+            alignItems: "center",
+            padding: 0,
+            margin: 0,
+            marginLeft: 5,
+            maxHeight: "8vh",
+            zIndex: 1,
+          }}
         >
+          {/* <InputLabel style={{color: "#fff"}}>
+          Categories
+          </InputLabel> */}
           {arrCategories.map((name) => (
             <MenuItem key={name} value={name}>
               <Checkbox checked={selectedCategories.indexOf(name) > -1} />
