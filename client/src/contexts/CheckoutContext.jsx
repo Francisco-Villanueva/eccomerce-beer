3b543ffea50f785bad7728291850f7d99da56f50
Francisco-Villanueva <franciscovillanuevaj99@gmail.com>
2023-09-14 17:37:56 -0300

    checkout desde el front

--- client/src/contexts/CheckoutContext.jsx
+++ client/src/contexts/CheckoutContext.jsx
@@ -1,3 +1,4 @@
+import axios from "axios";
 import { useState, createContext } from "react";
 
 // const initialState = {};
@@ -6,8 +7,20 @@ export const CheckoutContext = createContext();
 export const CheckoutContextProvider = ({ children }) => {
   const [addresses, setAddresses] = useState([]);
 
+  const checkOut = async (userId) => {
+    try {
+      const res = await axios.post(
+        `http://localhost:4000/user/checkout/${userId}`
+      );
+
+      console.log("checkOut!");
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
-    <CheckoutContext.Provider value={{ addresses, setAddresses }}>
+    <CheckoutContext.Provider value={{ addresses, setAddresses, checkOut }}>
       {children}
     </CheckoutContext.Provider>
   );
